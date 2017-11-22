b882bcbc2a55b1cf715fe70af1adc187526d05aa
jeremybonnard <jeremybonnard42@gmail.com>
2017-11-22 10:33:10 +0000

    Demande push

--- src/AppBundle/Controller/CrudController.php
+++ src/AppBundle/Controller/CrudController.php
@@ -6,7 +6,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use AppBundle\Entity\Book;
+use Symfony\Component\Form\Extension\Core\Type\TextType; 
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CrudController extends Controller
 {
@@ -15,44 +17,86 @@ class CrudController extends Controller
      */
 	    public function createAction(Request $request)
     {
-    	$livre = new Livre();
+    	$livre = new Book();
     	$form = $this->createFormBuilder($livre)
-    	->add('titre', TextType::class)
+    	->add('titre', TextType::class) 
+    	->add('save', SubmitType::class, array('label' => 'Creer Livre'))
   		->getForm();
+
+  		  $form->handleRequest($request);
+
+    	if ($form->isSubmitted() && $form->isValid()) {
+        	// $form->getData() holds the submitted values
+	        // but, the original `$task` variable has also been updated
+	        $livre = $form->getData();
+
+	        // ... perform some action, such as saving the task to the database
+	        // for example, if Task is a Doctrine entity, save it!
+	         $em = $this->getDoctrine()->getManager();
+	         $em->persist($livre);
+	         $em->flush();
+	        return $this->redirectToRoute('read');
+    	}	
         // replace this example code with whatever you need
-        return $this->render('AppBundle\Resources\views\CRUD\createView.html.twig', [
+        return $this->render('AppBundle:CRUD:createView.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
-		public function updateAction($id)
+	/**
+     * @Route("/update/{id}", name="update")
+     */
+		public function updateAction(Request $request, $id)
 	{
-		// $form = $this->createFormBuilder($livre)
-		// ->add('titre', TextType::class)
-		// ->getForm();
-			// replace this example code with whatever you need
-			return $this->render('AppBundle\Resources\views\CRUD\updateView.html.twig', [
-					'form' => $form->updateView(),
-			]);
+		$em = $this->getDoctrine()->getManager();
+		$livre = $em->getRepository(book::class)->find($id);
+    	$form = $this->createFormBuilder($livre)
+    	->add('titre', TextType::class) 
+    	->add('save', SubmitType::class, array('label' => 'Update le livre'))
+  		->getForm();
+
+  		  $form->handleRequest($request);
+
+    	if ($form->isSubmitted() && $form->isValid()) {
+        	// $form->getData() holds the submitted values
+	        // but, the original `$task` variable has also been updated
+	        $livre = $form->getData();
+
+	        // ... perform some action, such as saving the task to the database
+	        // for example, if Task is a Doctrine entity, save it!
+	         $em = $this->getDoctrine()->getManager();
+	         $em->persist($livre);
+	         $em->flush();
+	        return $this->redirectToRoute('read');
+    	}	
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:CRUD:updateView.html.twig', [
+            'form' => $form->createView(),
+            'livre' => $livre
+        ]);
 	}
+
+	/**
+     * @Route("/delete/{id}", name="delete")
+     */
 	public function deleteAction($id)
-{
-	// $form = $this->createFormBuilder($livre)
-	// ->add('titre', TextType::class)
-	// ->getForm();
-		// replace this example code with whatever you need
-		return $this->render('AppBundle\Resources\views\CRUD\deleteView.html.twig', [
-				'form' => $form->deleteView(),
+	{
+	    $em = $this->getDoctrine()->getManager();
+        $livre = $em->getRepository()->find($id);
+
+		return $this->render('AppBundle:CRUD:deleteView.html.twig', [
 		]);
-}
-public function readAction($id)
-{
-// $form = $this->createFormBuilder($livre)
-// ->add('titre', TextType::class)
-// ->getForm();
-	// replace this example code with whatever you need
-	return $this->render('AppBundle\Resources\views\CRUD\readView.html.twig', [
-			'form' => $form->readView(),
+	}
+
+	/**
+     * @Route("/read", name="read")
+     */
+	public function readAction()
+	{
+	    $em = $this->getDoctrine()->getManager();
+	    $livres = $em->getRepository(Book::class)->findAll();
+	return $this->render('AppBundle:CRUD:readView.html.twig', [
+	    'livres' => $livres
 	]);
-}
+	}
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,14 +2,13 @@
 
 namespace AppBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
-class User extends BaseUser
+class User extends \FOS\UserBundle\Model\User
 {
     /**
      * @ORM\Id
