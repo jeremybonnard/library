840b749fc53ab9d016a1cb907501c6eb7224c108
jeremybonnard <jeremybonnard42@gmail.com>
2017-11-23 15:31:03 +0000

    ajout plein de php

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-find ./ -exec curl -v -T {} ftp://enjolrasquentin:210295@ftp.enjolrasquentin.altervista.org/ \; -exec sleep 1 \;
+find ./ -exec curl -T {} ftp://enjolrasquentin:210295@ftp.enjolrasquentin.altervista.org/ \; -exec sleep 1 \;
