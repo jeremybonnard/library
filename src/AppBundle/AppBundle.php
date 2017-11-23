2edd1c7e686cac0268dd178f1b8d43a5634ee726
jeremybonnard <jeremybonnard42@gmail.com>
2017-11-23 10:52:47 +0000

    =0

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ *
+ */
 namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
--- src/AppBundle/Controller/CrudController.php
+++ src/AppBundle/Controller/CrudController.php
@@ -13,99 +13,107 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CrudController extends Controller
 {
-	/**
+    /**
      * @Route("/create", name="create")
      */
-	    public function createAction(Request $request)
+    public function createAction(Request $request)
     {
         dump($request);
-    	$livre = new Book();
-    	$form = $this->createFormBuilder($livre)
-    	->add('titre', TextType::class)
-    	->add('save', SubmitType::class, array('label' => 'Creer Livre'))
-  		->getForm();
+        $livre = new Book();
+        $form = $this->createFormBuilder($livre)
+            ->add('titre', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Creer Livre'))
+            ->getForm();
 
-  		  $form->handleRequest($request);
+        $form->handleRequest($request);
 
-    	if ($form->isSubmitted() && $form->isValid()) {
-    	    $livre = $form->getData();
-	        $em = $this->getDoctrine()->getManager();
-	        $em->persist($livre);
-	        $em->flush();
-	        return $this->redirectToRoute('read');
-    	}
+        if ($form->isSubmitted() && $form->isValid()) {
+            $livre = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($livre);
+            $em->flush();
+            return $this->redirectToRoute('read');
+        }
 
-        return $this->render('AppBundle:CRUD:createView.html.twig', [
+        return $this->render(
+            'AppBundle:CRUD:createView.html.twig', [
             'form' => $form->createView(),
-        ]);
+            ]
+        );
     }
 
-	/**
+    /**
      * @Route("/update/{id}", name="update")
      */
-		public function updateAction(Request $request, $id)
-	{
-		$em = $this->getDoctrine()->getManager();
-		$livre = $em->getRepository(book::class)->find($id);
-    	$form = $this->createFormBuilder($livre)
-    	->add('titre', TextType::class)
-    	->add('save', SubmitType::class, array('label' => 'Update le livre'))
-  		->getForm();
-  		$form->handleRequest($request);
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $livre = $em->getRepository(book::class)->find($id);
+        $form = $this->createFormBuilder($livre)
+            ->add('titre', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Update le livre'))
+            ->getForm();
+        $form->handleRequest($request);
 
-    	if ($form->isSubmitted() && $form->isValid()) {
-	        $livre = $form->getData();
-	         $em = $this->getDoctrine()->getManager();
-	         $em->persist($livre);
-	         $em->flush();
-	        return $this->redirectToRoute('read');
-    	}
+        if ($form->isSubmitted() && $form->isValid()) {
+            $livre = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($livre);
+            $em->flush();
+            return $this->redirectToRoute('read');
+        }
         // replace this example code with whatever you need
-        return $this->render('AppBundle:CRUD:updateView.html.twig', [
+        return $this->render(
+            'AppBundle:CRUD:updateView.html.twig', [
             'form' => $form->createView(),
             'livre' => $livre
-        ]);
-	}
+            ]
+        );
+    }
 
-	/**
+    /**
      * @Route("/delete/{id}", name="delete")
      */
-	public function deleteAction($id)
-	{
-	    $em = $this->getDoctrine()->getManager();
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
         $livre = $em->getRepository(book::class)->find($id);
         $livre->setEtat(0);
         $em->persist($livre);
         $em->flush();
-		return $this->render('AppBundle:CRUD:deleteView.html.twig', [
-		    'livre' => $livre
-		]);
-	}
+        return $this->render(
+            'AppBundle:CRUD:deleteView.html.twig', [
+            'livre' => $livre
+            ]
+        );
+    }
 
-	/**
+    /**
      * @Route("/read", name="read")
      */
-	public function readAction()
-	{
-	    $em = $this->getDoctrine()->getManager();
-	    $livres = $em->getRepository(Book::class)->findAll();
-	return $this->render('AppBundle:CRUD:readView.html.twig', [
-	    'livres' => $livres
-	]);
-	}
+    public function readAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $livres = $em->getRepository(Book::class)->findAll();
+        return $this->render(
+            'AppBundle:CRUD:readView.html.twig', [
+            'livres' => $livres
+            ]
+        );
+    }
 
-	public function getLivreById($id)
-	{
-		// En guise de test on passe à cette fonction l'id d'un bouquin qu'on a déjà et si la fonction renvoi rien, c'est qu'elle est bugguée
-	    $em = $this->getDoctrine()->getManager();
-      $livre = $em->getRepository(book::class)->find($id);
-			return $livre;
-	}
-	public function getLivreByTitre($titre)
-	{
-		// En guise de test on passe à cette fonction le titre d'un bouquin qu'on a déjà et si la fonction renvoi rien, c'est qu'elle est bugguée
-	    $em = $this->getDoctrine()->getManager();
-      $livre = $em->getRepository(book::class)->findByTitre($titre);
-			return $livre;
-	}
+    public function getLivreById($id)
+    {
+        // En guise de test on passe à cette fonction l'id d'un bouquin qu'on a déjà et si la fonction renvoi rien, c'est qu'elle est bugguée
+        $em = $this->getDoctrine()->getManager();
+         $livre = $em->getRepository(book::class)->find($id);
+        return $livre;
+    }
+    public function getLivreByTitre($titre)
+    {
+        // En guise de test on passe à cette fonction le titre d'un bouquin qu'on a déjà et si la fonction renvoi rien, c'est qu'elle est bugguée
+        $em = $this->getDoctrine()->getManager();
+         $livre = $em->getRepository(book::class)->findByTitre($titre);
+        return $livre;
+    }
 }
